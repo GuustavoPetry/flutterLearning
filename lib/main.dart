652c74a017938f2d2fce446fddb8ceb7a701aed0
GuustavoPetry <ogustavopetry@gmail.com>
2025-05-20 11:46:59 -0300

    primeiro app flutter

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(Container(
+    child: Text(
+      "Bem-vindo ao Flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.lightGreen, fontSize: 30.0),
+    ),
+  ));
+}

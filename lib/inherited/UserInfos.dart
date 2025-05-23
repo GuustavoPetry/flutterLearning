9c3b551d7c3f03ae6bf43d8b2266278b905cecd6
GuustavoPetry <ogustavopetry@gmail.com>
2025-05-23 08:54:08 -0300

    centralizando widget horizontalmente

--- lib/inherited/UserInfos.dart
+++ lib/inherited/UserInfos.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meu_app/inherited/UserData.dart';
+import 'package:meu_app/stateless/cardUser/UserCard.dart';
 
 class UserInfos extends StatelessWidget {
   @override
@@ -7,16 +8,22 @@ class UserInfos extends StatelessWidget {
     return UserData(
       username: "Gustavo",
       child: MaterialApp(
-
         home: Scaffold(
-        appBar: AppBar(title: Text("Perfil de usuário")),
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, 
-        children: [
-          WelcomeMessage(),
-          SizedBox(height: 20,)
-        ],
+          appBar: AppBar(title: Text("Perfil de usuário")),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                UserCard(
+                  nome: "Gustavo Petry",
+                  email: "ogustavopetry@gmail.com",
+                  avatarUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdwT12D_59OK-CMrBN5BrcrGjdzT4MSavJ6A&s",
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
       ),
     );
   }
@@ -26,6 +33,6 @@ class WelcomeMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final username = UserData.of(context).username;
-    return Text("Bem vindo $username", style: TextStyle(fontSize: 24),);
+    return Text("Bem vindo $username", style: TextStyle(fontSize: 24));
   }
 }
